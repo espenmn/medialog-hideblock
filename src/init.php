d33d71c4628f2fbda2cd4ebd70d0ba3ac91452e8
Espen Moe-Nilssen <espen@medialog.no>
2023-07-25 14:09:37 +0200

    Update init.php

--- src/init.php
+++ src/init.php
@@ -32,11 +32,14 @@ function medialog_hideblock_assets() { // phpcs:ignore
 		null // filemtime( plugin_dir_path( __DIR__ ) . 'dist/blocks.editor.build.css' ) // Version: File modification time.
 	);
 
-
 	register_block_type(
 		'medialog/block-medialog-hideblock', array(
+			// Enqueue blocks.style.build.css on both frontend & backend.
+			//'style'         => 'medialog_hideblock-style-css',
 			// Enqueue blocks.build.js in the editor only.
 			'editor_script' => 'medialog_hideblock-js',
+			// Enqueue blocks.editor.build.css in the editor only.
+			'editor_style'  => 'medialog_hideblock-editor-css',
 			'render_callback' => 'medialog_hideblock_render',
 			'attributes' => [
 					'disableBlock' => [
@@ -55,13 +58,29 @@ add_action( 'init', 'medialog_hideblock_assets' );
 // Code to disable block from rendering if bool field 'disabled' is True
 // Overrides the default rendering
 function medialog_block_wrapper( $block_content, $block ) {
-	//$disabled = false;
-	if ( isset( $block['attrs']['disableBlock'] ) && $block['attrs']['disableBlock'] ) {
-		return '';
-	} else {
+	$disabled = false;
+	if (isset($block['attrs'])) {
+		$myAttr = $block['attrs'];
+		$myKeys = array_keys($myAttr);
+
+
+		// Hack around multidimentional array problem
+		foreach ($myKeys as $myKey) {
+			$myValue = $myAttr[$myKey];
+			if ($myKey == 'disableBlock' ){
+				if ($myAttr[$myKey] != false) {
+					$disabled = true;
+				}
+			}
+	}}
+
+	if (!$disabled){
+		//return content the normal way;
 		return $block_content;
 	}
-
+	// Hide block;
+	// Alternatively, we could show it only for admins
+	return '';
 }
 
 add_filter( 'render_block', 'medialog_block_wrapper', 10, 2 );
